8c8e3ff49156eaf165208e5d5660863ef0b7f1e4
CGM123sherry <g.christinemurithi@gmail.com>
2024-08-11 11:56:23 +0300

    completed tdd writing test lab

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+function isPalindrome(word) {
+  if (typeof word !== "string") {
+    throw new Error("Invalid input: input must be a string");
+  }
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error("Invalid input: only alphabetic characters are allowed");
+  }
+  const cleanedWord = word.toLowerCase();
+  return cleanedWord === cleanedWord.split("").reverse().join("");
+}
+
+module.exports = { isPalindrome };
